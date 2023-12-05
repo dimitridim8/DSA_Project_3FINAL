8c43aedefb80e48be863da4bbf478187904a9bc9
uhussain11 <117589268+uhussain11@users.noreply.github.com>
2023-12-04 23:40:40 -0500

    Update VectorGraph.h

--- DSA_Project3/VectorGraph.h
+++ DSA_Project3/VectorGraph.h
@@ -1,8 +1,7 @@
 #pragma once
 #include "GameInfo.h"
 #include <vector>
-#include <algorithm>
-#include <iostream>
+#include <string>
 
 class VectorGraph {
 private:
@@ -10,12 +9,16 @@ private:
 
 public:
     void addGame(const GameInfo& game);
-    void sortGames();
-    void displayGames() const;
-    void filterAndDisplayTopGames(const std::string& region, bool globalPopularity) const;
+    void sortGames(); // Method to sort games
+
+    // Method to filter games based on criteria
     std::vector<GameInfo> filterGames(const std::string& genre, const std::string& publisher,
                                       const std::string& platform, int year) const;
-    void sortAndDisplayGames(const std::vector<GameInfo>& filteredGames, bool globalPopularity) const;
+
+    // Method for flexible filtering
     std::vector<GameInfo> filterGamesFlexibly(const std::string& genre, const std::string& publisher,
                                               const std::string& platform, int year) const;
+
+    // Method to sort and display games
+    void sortAndDisplayGames(const std::vector<GameInfo>& games, bool globalPopularity) const;
 };
